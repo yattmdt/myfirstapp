4077fa4d60988c97d6e6a4bd8959686d6bd368df
yattmdt <154412019+yattmdt@users.noreply.github.com>
2023-12-20 16:23:16 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 
 st.header("My first Streamlit App")
+
 st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
 show = st.checkbox('I agree the terms and conditions')
 if show:
